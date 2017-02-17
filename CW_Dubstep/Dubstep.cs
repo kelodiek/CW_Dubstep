41efd34ca3bde9bdc84a7fb1c5f342eb736b8a56
Elodie <bluepaper324419@gmail.com>
2017-02-16 21:47:22 -0500

    Ajout projet CW_Dubstep

---
+++ CW_Dubstep/Dubstep.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CW_Dubstep
+{
+    class Dubstep
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static string SongDecoder(string input)
+        {
+            string response = "";
+            Array.ForEach(input.Replace("WUB", " ").Trim().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), x => { response = response + x + " ";  });
+            return response.Trim();
+        }
+    }
+}
